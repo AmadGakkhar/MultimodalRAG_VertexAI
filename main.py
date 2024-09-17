3f09ad625bdb986e8799e7e99ef70edcdd7e9342
AmadUdDinGakkhar <a_gakkhar@hotmail.com>
2024-09-17 19:58:29 +0500

    prescription parsing complete

--- main.py
+++ main.py
@@ -4,16 +4,60 @@ import csv
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+from pydantic import BaseModel
+import json
 
 load_dotenv()
 
 
-def get_engine_numbers(images, image_names):
-    engineNumbers = []
-    imageNames = []
+class Prescription(BaseModel):
+    hospital_name: str
+    doctor_name: str
+    patient_name: str
+    gender: str
+    age: str
+    diagnosis: str
+    medication: str
+
+
+def get_model():
+
     genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
     model = genai.GenerativeModel("gemini-1.5-flash")
+    return model
+
+
+def get_prescription(model, images, image_names):
+    json_data = []
+    prompt = """
+    
+    The image is a doctor's prescription. 
+    Please extract the following information.
+    
+    Hospital/Clinic Name, Doctor Name, Patient Name, Gender, Age, Diagnosis, Medication
+    if any information is not present return null.
+    
+    """
+
+    for image_name, image in zip(image_names, images):
+        response = model.generate_content(
+            [prompt, image],
+            generation_config=genai.GenerationConfig(
+                response_mime_type="application/json", response_schema=Prescription
+            ),
+        )
+        # print(response.text)
+        json_item = Prescription.model_validate(json.loads(response.text)).model_dump()
+        json_data.append(json_item)
+    print(json_data)
+    print(type(json_data[0]))
+    json_to_csv(json_data=json_data, csv_filename=file_path_prescription)
+
+
+def get_engine_numbers(model, images, image_names):
+    engineNumbers = []
+    imageNames = []
     prompt = """
         
         The image is a close up of an engine. Please extract the engine number from it. 
@@ -23,7 +67,6 @@ def get_engine_numbers(images, image_names):
         response = model.generate_content([prompt, image])
         engineNumbers.append(response.text.strip())
         imageNames.append(image_name)
-        # print(engineNumbers)
     write_csv(image_names=imageNames, extracted_text=engineNumbers, filename=file_path)
 
 
@@ -50,8 +93,32 @@ def write_csv(image_names, extracted_text, filename):
             writer.writerow({"image_Name": image_name, "engine_Number": text})
 
 
-folder_path = "/home/amadgakkhar/code/MultimodalRAG_VertexAI/Images"
-file_path = "/home/amadgakkhar/code/MultimodalRAG_VertexAI/image_text_extraction.csv"
+def json_to_csv(json_data, csv_filename):
+    """Writes JSON data to a CSV file.
 
-image_names, images = get_images(folder_path=folder_path)
-get_engine_numbers(image_names=image_names, images=images)
+    Args:
+      json_data: A list of JSON objects.
+      csv_filename: The name of the CSV file to create.
+    """
+
+    # Get the field names from the first JSON object
+    fieldnames = json_data[0].keys()
+
+    with open(csv_filename, "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for data in json_data:
+            writer.writerow(data)
+
+
+folder_path_engine = "/home/amadgakkhar/code/MultimodalRAG_VertexAI/engine"
+folder_path_prescription = "/home/amadgakkhar/code/MultimodalRAG_VertexAI/prescription"
+file_path_engine = "/home/amadgakkhar/code/MultimodalRAG_VertexAI/engine_numbers.csv"
+file_path_prescription = (
+    "/home/amadgakkhar/code/MultimodalRAG_VertexAI/prescriptions.csv"
+)
+
+
+image_names, images = get_images(folder_path=folder_path_prescription)
+get_prescription(model=get_model(), image_names=image_names, images=images)
--- prompt.py
+++ prompt.py
@@ -4,3 +4,23 @@ Extract Text from this image
 Your output should be of the form
 
 """
+PRESCRIPTION_PROMPT = """
+    
+    The image is a doctor's prescription. 
+    Please extract the following information.
+    
+    Hospital/Clinic Name, Doctor Name, Patient Name, Gender, Age, Diagnosis, Medication
+    if any information is not present return null.
+    
+    Your output should be in JSON format
+    Expected Output Format:
+    {
+    "hospital_name": "",
+    "doctor_name": "",  
+    "patient_name": "",
+    "gender": "",
+    "age": "",
+    "diagnosis": "",
+    "medication": ""
+    }     
+    """
